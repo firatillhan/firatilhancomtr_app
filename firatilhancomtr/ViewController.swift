55a06b0295e93befcf476705b4b0bc0533d9cdbd
firatillhan <firatilhan008@gmail.com>
2026-05-27 00:27:25 +0300

    Initial Commit

---
+++ firatilhancomtr/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  firatilhancomtr
+//
+//  Created by Fırat İlhan on 27.05.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
