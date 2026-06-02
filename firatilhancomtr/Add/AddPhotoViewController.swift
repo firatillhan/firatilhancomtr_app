9d2006d6ab7c0567ce78a40b2e442601d4bded68
firatillhan <firatilhan008@gmail.com>
2026-06-02 23:26:48 +0300

    photo add view controller page added and ui complated.

---
+++ firatilhancomtr/Add/AddPhotoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddPhotoViewController.swift
+//  firatilhancomtr
+//
+//  Created by Fırat İlhan on 2.06.2026.
+//
+
+import UIKit
+
+class AddPhotoViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var photoNameLabel: UITextField!
+    @IBOutlet weak var photoDetailLabel: UITextField!
+    @IBOutlet weak var photoLocationLabel: UITextField!
+    @IBOutlet weak var photoCityLabel: UITextField!
+
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+
+}
